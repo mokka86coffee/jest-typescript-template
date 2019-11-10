409716d54e2f6f862930d50bb6b05ceb8294afbc
Evgeniy_Marushev <emarushev@eshe.studio>
2019-11-10 18:23:01 +0300

    additional

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,23 +5,72 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a shallowWrapper for the App component.
+ * @function setup
+ * @param {any} state
+ * @param {Object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (state = null, props = {}):Enzyme.ShallowWrapper => (
+  shallow(<App {...props}/>)
+);
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {string} value - string selector 
+ * @param {ShallowWrapper} wrapper - Enzyme ShallowWrapper to search within
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (value: string, wrapper?: Enzyme.ShallowWrapper): Enzyme.CommonWrapper => {
+  const shallowWrapper = wrapper || setup();
+  return shallowWrapper.find(`[data-test='${value}']`);
+}
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find("[data-test='component-app']").length).toBe(2);
+  expect(findByTestAttr('component-App').length).toBe(1);
 });
 
 test('renders increment button', () => {
+  expect(findByTestAttr('component-increase-button').length).toBe(1);
+});
 
+test('renders decrease button', () => {
+  expect(findByTestAttr('component-decrease-button').length).toBe(1);
 });
 
 test('renders counter display', () => {
-
+  expect(findByTestAttr('component-counter-display').length).toBe(1);
 });
 
 test('counter start from 0', () => {
-
+  expect(setup().state('counter')).toBe(0);
 });
 
-test('clicking button increments counter', () => {
+describe('test incrementing / decrementing', () => {
+  const wrapped = setup();
+  const buttonIncrease = findByTestAttr('component-increase-button', wrapped);
+  const buttonDecrease = findByTestAttr('component-decrease-button', wrapped);
+
+  test('clicking button increments counter', () => {
+    buttonIncrease.simulate('click');
+    expect(wrapped.state('counter')).toBe(1);
+  });
+  
+  test('render new value after clicking increment', () => {
+    const display = findByTestAttr('component-counter-display', wrapped);
+    expect(display.text()).toContain('1');
+  });
+
+  test('clicking button decrements counter', () => {
+    buttonDecrease.simulate('click');
+    expect(wrapped.state('counter')).toBe(0);
+  });
+
+  test('render error if decrement button clicked and the counter value is 0', () => {
+    buttonDecrease.simulate('click');
+    const display = findByTestAttr('component-counter-display', wrapped);
+    expect(display.text()).toContain('Below 0, can\'t do it :)');
+  });
 
 });
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ interface IPropTypes {
 }
 
 interface IStateTypes {
-  counter: number
+  counter: number | string
 }
 
 class App<IPropTypes, IStateTypes> extends React.Component {
@@ -16,13 +16,34 @@ class App<IPropTypes, IStateTypes> extends React.Component {
     counter: 0
   }
 
+  operation = (type: string) => {
+    const { counter } = this.state;
+    const isError = typeof counter === 'string';
+
+    if (type === 'increase') {
+      const trueValue = isError ? 0 : counter;
+      this.setState({ counter: trueValue + 1 });
+      return;
+    }
+
+    if (isError) {
+      return;
+    }
+
+    this.setState({
+      counter: counter === 0 ? 'Below 0, can\'t do it :)' : counter - 1
+    });
+  }
+
   render() {
+    const {counter} = this.state;
     return (
-      <div data-test="component-pp" className="App">
+      <div data-test="component-App" className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          </p>
+          <p data-test="component-counter-display">Counter {counter}</p>
+          <button data-test="component-decrease-button" onClick={() => this.operation('decrease')}>Decrease</button>
+          <button data-test="component-increase-button" onClick={() => this.operation('increase')}>Increase</button>
         </header>
       </div>
     );
